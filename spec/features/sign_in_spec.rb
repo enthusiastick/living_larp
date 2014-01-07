db8b027abf54bba246cf6806bf56e4d82bafc98a
Eben <eben.lowe@gmail.com>
2014-01-06 20:33:31 -0500

    Sign-in spec complete, unhappy path tests green.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -23,8 +23,43 @@ feature "User signs in", %Q{
     expect(page).to have_content('Sign Out')
   end
 
-  scenario 'an unregistered user signs in'
+  scenario 'an unregistered user signs in' do
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: 'whoever@example.com'
+    fill_in 'Password', with: '1234'
 
-  scenario 'an authenticated user can\'t sign in again'
+    click_button 'Sign In'
+    expect(page).to_not have_content('Welcome')
+    expect(page).to_not have_content('Sign Out')
+    expect(page).to have_content('Invalid')
+  end
+
+  scenario 'existing user with wrong password' do
+    user = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'wrong'
+
+    click_button 'Sign In'
+    expect(page).to_not have_content('Welcome')
+    expect(page).to_not have_content('Sign Out')
+    expect(page).to have_content('Invalid')
+  end
+
+  scenario 'an authenticated user can\'t sign in again' do
+    user = FactoryGirl.create(:user)
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_button 'Sign In'
+    expect(page).to have_content('Sign Out')
+    expect(page).to_not have_content('Sign In')
+
+    visit new_user_session_path
+    expect(page).to have_content('already signed in')
+  end
 
 end
