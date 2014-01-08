d57d652ef9801e565b40e6e624a98ccdaefeb692
Eben <eben.lowe@gmail.com>
2014-01-07 20:24:05 -0500

    Updated to fully match the user story; spec passing.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,15 @@
 class Contact < MailForm::Base
-  attribute :name, validate: true
+  attribute :first_name, validate: true
+  attribute :last_name, validate: true
+  attribute :subject, validate: true
   attribute :email, validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
 
   def headers
     {
-      :subject => "Living LARP Contact Form",
+      :subject => %("#{subject}"),
       :to => "contact@example.com",
-      :from => %("#{name}" <#{email}>)
+      :from => %("#{first_name} #{last_name}" <#{email}>)
     }
   end
 end
--- spec/features/contact_form_spec.rb
+++ spec/features/contact_form_spec.rb
@@ -16,14 +16,16 @@ feature "User signs in", %Q{
   scenario "with all correct info" do
     ActionMailer::Base.deliveries = []
     visit new_contact_path
-    fill_in 'Name', with: 'Clara Oswald'
+    fill_in 'First', with: 'Clara'
+    fill_in 'Last', with: 'Oswald'
     fill_in 'Email', with: 'foo@example.com'
+    fill_in 'Subject', with: 'time and space'
     fill_in 'Message', with: 'help i am trapped in the time vortex'
     click_button 'Send Message'
 
     expect(ActionMailer::Base.deliveries.size).to eq(1)
     last_email = ActionMailer::Base.deliveries.last
-    expect(last_email).to have_subject('Living LARP Contact Form')
+    expect(last_email).to have_subject(/time and space/)
     expect(last_email).to deliver_to('contact@example.com')
     expect(last_email).to have_body_text(/help i am trapped in the time vortex/)
   end
