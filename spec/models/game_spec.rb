ac68ce315360e18497846cb82d45a55f313295ba
Eben <eben.lowe@gmail.com>
2014-01-09 09:03:30 -0500

    Unit tests for game trait, improved tests for game.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -9,14 +9,18 @@ describe Game do
   end
 
   describe "Validation Tests" do
+    FactoryGirl.create(:game)
     it { should have_valid(:name).when("Blah blah") }
     it { should_not have_valid(:name).when(nil, "") }
+    it { should validate_uniqueness_of(:name) }
     it { should have_valid(:starting_points).when(9) }
     it { should_not have_valid(:starting_points).when(nil, "") }
   end
 
   describe "Association Tests" do
     it { should belong_to(:user) }
+    it { should have_many(:game_traits) }
+    it { should have_many(:characters) }
   end
 
 end
---
+++ spec/models/game_trait_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe GameTrait do
+
+  describe "Database Tests" do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:max_purchases).of_type(:integer) }
+    it { should have_db_column(:bgs).of_type(:boolean) }
+    it { should have_db_column(:point_cost).of_type(:integer) }
+    it { should have_db_column(:game_id).of_type(:integer) }
+  end
+
+  describe "Validation Tests" do
+    it { should have_valid(:name).when("Blah blah") }
+    it { should_not have_valid(:name).when(nil, "") }
+    it { should have_valid(:point_cost).when(9) }
+    it { should_not have_valid(:point_cost).when(nil, "") }
+  end
+
+  describe "Association Tests" do
+    it { should belong_to(:game) }
+  end
+
+end
