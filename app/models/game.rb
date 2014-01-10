30043e54eec71d970d7441d7ae3b28f790b62669
Eben <eben.lowe@gmail.com>
2014-01-09 19:58:37 -0500

    Numericality validations on game starting points and game trait point costs (integer-only, >0.)

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ActiveRecord::Base
   validates_presence_of :name, :starting_points
+  validates_numericality_of :starting_points, integer_only: true, greater_than: 0
   validates_uniqueness_of :name
   belongs_to :user
   has_many :game_traits
--- app/models/game_trait.rb
+++ app/models/game_trait.rb
@@ -1,5 +1,6 @@
 class GameTrait < ActiveRecord::Base
   validates_presence_of :name, :point_cost, :game_id
+  validates_numericality_of :point_cost, integer_only: true, greater_than: 0
   belongs_to :game
   has_many :traits
   has_many :characters, through: :traits
