f101860224d7f8a18f15767cb8b2e8e287f6761c
Eben <eben.lowe@gmail.com>
2014-01-13 19:29:17 -0500

    Other acceptance tests for core feature (add, not invite, players) passing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,5 @@ class User < ActiveRecord::Base
   def name
     self.email
   end
+
 end
--- spec/features/user_invites_players_to_game_spec.rb
+++ spec/features/user_invites_players_to_game_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "User configures a game", %Q{
+feature "User adds players to a game", %Q{
   As an authenticated user with a valid game
   I want to invite other users to play the game
   so that they can create characters
@@ -18,15 +18,38 @@ feature "User configures a game", %Q{
     user2 = FactoryGirl.create(:user)
     game = FactoryGirl.create(:game, user: user1)
     login(user1)
-    visit game_path(game)
-    click_on "Add Player"
+    visit game_players_path(game)
     fill_in "Email", with: user2.email
     click_on "Add Player"
+    click_on "Confirm"
 
     expect(page).to have_content("success")
     expect(Player.count).to eq(count + 1)
     expect(Player.last.user).to eq(user2)
     expect(Player.last.game).to eq(game)
+  end
+
+  scenario "invalid user" do
+    count = Player.count
+    user = FactoryGirl.create(:user)
+    game = FactoryGirl.create(:game, user: user)
+    login(user)
+    visit game_players_path(game)
+    fill_in "Email", with: "puffthemagicdragon@example.com"
+    click_on "Add Player"
+
+    expect(page).to have_content("not found")
+    expect(Player.count).to eq(count)
+  end
+
+  scenario "view players index" do
+    game = FactoryGirl.create(:game)
+    player1 = FactoryGirl.create(:player, game: game)
+    player2 = FactoryGirl.create(:player, game: game)
+    visit game_players_path(game)
+
+    expect(page).to have_content(player1.user.name)
+    expect(page).to have_content(player2.user.name)
   end
 
 end
