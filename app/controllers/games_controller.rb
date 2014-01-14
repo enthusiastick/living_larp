cc42e0cd312acb1d2df043cb07059a0055c0ee00
Eben <eben.lowe@gmail.com>
2014-01-13 20:12:39 -0500

    Fixed? problem with adding players system.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,10 @@
 class GamesController < ApplicationController
 
   before_action :authenticate_user, only: [:new, :create]
-  before_action :set_user, only: [:create, :index, :show]
 
   def create
     @game = Game.new(game_params)
-    @game.user = @user
+    @game.user = current_user
     if @game.save
       flash['alert-box success'] = "Game created successfully."
       redirect_to game_path(@game)
@@ -23,10 +22,10 @@ class GamesController < ApplicationController
   end
 
   def show
-    if @user == nil
+    if current_user == nil
       raise ActionController::RoutingError.new('Not Found')
     else
-      @game = @user.games.find(params[:id])
+      @game = current_user.games.find(params[:id])
     end
   end
 
