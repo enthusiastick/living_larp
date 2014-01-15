6d928b5b08f42ca49271ff9d5a42c8f11df505e7
Eben <eben.lowe@gmail.com>
2014-01-15 14:01:51 -0500

    Set up SendGrid for staging app.

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -12,8 +12,13 @@ if Rails.env.staging?
 
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    address: "localhost",
-    port: 1025
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
 
 end
