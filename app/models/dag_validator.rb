aebb8855fe579a21f94fc4dba59cad2492b82fc9
Eben <eben.lowe@gmail.com>
2014-01-19 13:22:34 -0500

    Trying to extend TSort to validate acyclicity of graph. DAG model works, but can't correctly include it in DAGVAlidator.

--- app/models/dag_validator.rb
+++ app/models/dag_validator.rb
@@ -1,23 +1,41 @@
 class DagValidator < ActiveModel::Validator
 
-  def validate(record)
-    parent = record.parent_trait
-    child = record.child_trait
-    child_visited = explore(child, [])
-    binding.pry
-    if child_visited.include?(parent)
-      record.errors[:child] << 'This creates a prerequisites loop.'
-    end
-  end
+  require 'd_a_g'
+  include DAG
 
-  def explore(trait, visited)
-    visited << trait
-    trait.child_traits.each do |child|
-      unless visited.include?(child)
-        visited = explore(child, visited)
+  def validate(record)
+    graph = DAG.new
+    graph[record.parent_trait] = [record.child_trait]
+    record.child_trait.game.game_traits.each do |trait|
+      trait.game_trait_depencies.each do |dependency|
+        if graph[dependency.parent_trait] = nil
+          graph[dependency.parent_trait] = dependency.child_trait
+        else
+          graph[dependency.parent_trait] << dependency.child_trait
+        end
       end
     end
-    visited
+    binding.pry
   end
+
+  # def validate(record)
+  #   parent = record.parent_trait
+  #   child = record.child_trait
+  #   child_visited = explore(child, [])
+  #   binding.pry
+  #   if child_visited.include?(parent)
+  #     record.errors[:child] << 'This creates a prerequisites loop.'
+  #   end
+  # end
+
+  # def explore(trait, visited)
+  #   visited << trait
+  #   trait.child_traits.each do |child|
+  #     unless visited.include?(child)
+  #       visited = explore(child, visited)
+  #     end
+  #   end
+  #   visited
+  # end
 
 end
